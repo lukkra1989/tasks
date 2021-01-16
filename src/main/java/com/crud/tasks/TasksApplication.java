fb5543f1ef8916c3d6662ac7e99673b880c99597
lukkra1989 <luk.kramarczyk@gmail.com>
2021-01-16 16:29:07 +0100

    Initial commit2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -13,9 +13,6 @@ public class TasksApplication {
 
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-
         SpringApplication.run(TasksApplication.class, args);
     }
 
@@ -23,13 +20,12 @@ public class TasksApplication {
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //        return application.sources(TasksApplication.class);
 //    }
-=======
+
 //public class TasksApplication extends SpringBootServletInitializer {
-        SpringApplication.run(TasksApplication.class, args);
 //	@Override
 //	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
 //		return application.sources(TasksApplication.class);
 //	}
     }
->>>>>>> bd015cd2a49d2f641f1651effe854f117a082161
-}
+
+
