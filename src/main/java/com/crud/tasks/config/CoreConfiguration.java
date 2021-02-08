96842c4d2f7773116b4d5e6d2abd5f68d807e0bc
lukkra1989 <luk.kramarczyk@gmail.com>
2021-02-08 17:13:00 +0100

    zad 23.2

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -1,6 +1,5 @@
 package com.crud.tasks.config;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 
 @EnableSwagger2
 @Configuration
@@ -24,7 +22,7 @@ public class CoreConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
+++ src/main/java/com/crud/tasks/controller/TaskNotFoundException.java
@@ -1,4 +1,7 @@
 package com.crud.tasks.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/v1/exception/")
 public class TaskNotFoundException extends Exception{
 }
